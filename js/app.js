5f08301e87eb34b660be49168258deff3d1e697b
njuno <njunoapps@gmail.com>
2019-10-12 11:57:23 +0300

    hshs

--- js/app.js
+++ js/app.js
@@ -1,19 +1,16 @@
-let duration = 60
-let textNumber = Math.floor(Math.random() * 3)
+let duration = 60;
+let textNumber = Math.floor(Math.random() * 3);
 
 events.init(duration, textNumber, false);
 
 document.querySelector("#restart-btn").addEventListener("click", restartTest);
 
-
 function restartTest() {
-    events.init(duration, textNumber, true);
+  events.init(duration, textNumber, true);
 }
 
-
-
-
-$("#menu-toggle").click(function (e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  document.querySelector("#sidebar-wrapper").style.display = "block";
+  $("#wrapper").toggleClass("toggled");
 });
